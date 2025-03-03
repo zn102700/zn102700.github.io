b1128b8245d86dda1ec189e0dae5a563e2f09bc4
zn102700 <15146485426@163.com>
2025-03-03 20:14:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ noButton.addEventListener("click", function () {
 });
 
 // Yes 按钮点击后，进入表白成功页面
-const loveTest = `!!!喜欢你!! ( >᎑<)♡︎ᐝ  ${
+const loveTest = `!!!赵理阳我喜欢你!! ( >᎑<)♡︎ᐝ  ${
   username ? `${safeUsername}  ♡︎ᐝ(>᎑< )` : ""
 }`;
 
